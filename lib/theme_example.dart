602befdb65f560df5d84dc13e90fc9ae5acb06b0
Hugo García Benjumea <hugogaben8.02@gmail.com>
2022-12-14 14:18:09 +0100

    added theme example

--- lib/theme_example.dart
+++ lib/theme_example.dart
@@ -1,23 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:timetracker_app/colors.dart';
-import 'package:timetracker_app/dark_theme.dart';
-import 'package:timetracker_app/light_theme.dart';
-import 'package:timetracker_app/page_activities.dart';
-
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'TimeTracker',
-        theme: _kShrineTheme,
-        home: const PageActivities(0));
-  }
-}
 
 // TODO: Build a Shrine Theme (103)
 final ThemeData _kShrineTheme = _buildShrineTheme();
