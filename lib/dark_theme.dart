6f0510122db8cdf4d4483f1ee99706aa9188780b
Hugo García Benjumea <hugogaben8.02@gmail.com>
2022-12-14 13:37:59 +0100

    update themes

--- lib/dark_theme.dart
+++ lib/dark_theme.dart
@@ -3,47 +3,15 @@ import 'package:flutter/material.dart';
 class DarkTheme {
   DarkTheme._();
 
-  static const Color _iconColor = Colors.orange;
-  // static const Color _darkPrimaryColor = Colors.black;
-  static const Color _darkSecondaryColor = Colors.white;
-  static const Color _darkOnPrimaryColor = Colors.white;
+  static const _primaryColor = Colors.orange;
 
   static final ThemeData darkTheme = ThemeData(
     useMaterial3: true,
-    // scaffoldBackgroundColor: _darkPrimaryColor,
-    appBarTheme: const AppBarTheme(
-        iconTheme: IconThemeData(
-      color: _iconColor,
-    )),
     colorScheme: const ColorScheme.dark(
-      primary: _iconColor,
-      secondary: _darkSecondaryColor,
-      onPrimary: _darkOnPrimaryColor,
+      primary: _primaryColor,
     ),
     iconTheme: const IconThemeData(
-      color: _iconColor,
+      color: _primaryColor,
     ),
-    textTheme: _darkTextTheme,
-  );
-
-  static const TextTheme _darkTextTheme = TextTheme(
-    headline1: _darkScreenHeadingTextStyle,
-    bodyText1: _darkScreenTaskNameStyle,
-    bodyText2: _darkScreenTaskDurationStyle,
-  );
-
-  static const TextStyle _darkScreenHeadingTextStyle = TextStyle(
-    fontSize: 48.0,
-    color: _darkOnPrimaryColor,
-  );
-
-  static const TextStyle _darkScreenTaskNameStyle = TextStyle(
-    fontSize: 20.0,
-    color: _darkOnPrimaryColor,
-  );
-
-  static const TextStyle _darkScreenTaskDurationStyle = TextStyle(
-    fontSize: 16.0,
-    color: _darkOnPrimaryColor,
   );
 }
--- lib/light_theme.dart
+++ lib/light_theme.dart
@@ -3,49 +3,15 @@ import 'package:flutter/material.dart';
 class LightTheme {
   LightTheme._();
 
-  static const Color _iconColor = Colors.yellow;
-  static const Color _lightPrimaryColor = Colors.pinkAccent;
-  static const Color _lightPrimaryVariantColor = Colors.blue;
-  static const Color _lightSecondaryColor = Colors.green;
-  static const Color _lightOnPrimaryColor = Colors.black;
+  static const _primaryColor = Colors.blue;
 
   static final ThemeData lightTheme = ThemeData(
     useMaterial3: true,
-    scaffoldBackgroundColor: _lightPrimaryVariantColor,
-    appBarTheme: const AppBarTheme(
-        color: _lightPrimaryVariantColor,
-        iconTheme: IconThemeData(
-          color: _lightOnPrimaryColor,
-        )),
     colorScheme: const ColorScheme.light(
-      primary: _lightPrimaryColor,
-      secondary: _lightSecondaryColor,
-      onPrimary: _lightOnPrimaryColor,
+      primary: _primaryColor,
     ),
     iconTheme: const IconThemeData(
-      color: _iconColor,
+      color: _primaryColor,
     ),
-    textTheme: _lightTextTheme,
-  );
-
-  static const TextTheme _lightTextTheme = TextTheme(
-    headline1: _lightScreenHeadingTextStyle,
-    bodyText1: _lightScreenTaskNameStyle,
-    bodyText2: _lightScreenTaskDurationStyle,
-  );
-
-  static const TextStyle _lightScreenHeadingTextStyle = TextStyle(
-    fontSize: 48.0,
-    color: _lightOnPrimaryColor,
-  );
-
-  static const TextStyle _lightScreenTaskNameStyle = TextStyle(
-    fontSize: 20.0,
-    color: _lightOnPrimaryColor,
-  );
-
-  static const TextStyle _lightScreenTaskDurationStyle = TextStyle(
-    fontSize: 16.0,
-    color: _lightOnPrimaryColor,
   );
 }
