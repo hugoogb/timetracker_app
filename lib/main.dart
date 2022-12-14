fc063f2d9980010747d4f4cad6ba4d61f1c08249
Hugo García Benjumea <hugogaben8.02@gmail.com>
2022-12-14 15:13:42 +0100

    change theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:timetracker_app/colors.dart';
 import 'package:timetracker_app/dark_theme.dart';
 import 'package:timetracker_app/light_theme.dart';
 import 'package:timetracker_app/page_activities.dart';
@@ -14,25 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'TimeTracker',
-        theme: _kShrineTheme,
+        theme: LightTheme.lightTheme,
+        darkTheme: DarkTheme.darkTheme,
         home: const PageActivities(0));
   }
-}
-
-// TODO: Build a Shrine Theme (103)
-final ThemeData _kShrineTheme = _buildShrineTheme();
-
-ThemeData _buildShrineTheme() {
-  final ThemeData base = ThemeData.light();
-  return base.copyWith(
-    colorScheme: base.colorScheme.copyWith(
-      primary: kShrinePink100,
-      onPrimary: kShrineBrown900,
-      secondary: kShrineBrown900,
-      error: kShrineErrorRed,
-    ),
-    // TODO: Add the text themes (103)
-    // TODO: Add the icon themes (103)
-    // TODO: Decorate the inputs (103)
-  );
 }
